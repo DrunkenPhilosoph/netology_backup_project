f0f6c97e6448d8cbe976821663415c5d411ecd92
Drunken_Philosopher <pepiko@yandex.ru>
2023-10-19 19:00:04 +0300

    change main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
+import pprint
 
 class VK:
 
-   def __init__(self, access_token, user_id, version='5.131'):
+   def __init__(self, access_token, version='5.131'):
        self.token = access_token
-       self.id = user_id
        self.version = version
        self.params = {'access_token': self.token, 'v': self.version}
 
@@ -12,12 +12,14 @@ class VK:
        url = 'https://api.vk.com/method/users.get'
        params = {'user_ids': self.id}
        response = requests.get(url, params={**self.params, **params})
+       print(self.params)
        return response.json()
-   def get_photo(self, photo_size, album_id, ):
-       url = f'https://api.vk.com/method/{}?owner_id={}&offset={}&count=200&v=5.131&access_token={}&v=5.131'
+   def get_photo(self, owner_id):
+       url = f'https://api.vk.com/method/photos.get?owner_id={owner_id}&album_id=profile&photo_sizes=o&&access_token={self.token}&v={self.version}'
+       response = requests.get(url)
+       pprint.pprint(response.json())
 
 
-access_token = ''
-user_id = '596289942'
-vk = VK(access_token, user_id)
-print(vk.users_info())
+access_token = 'vk1.a.QY5W0U2hK6wrFcPsVeLI7SjwAub_-xhx6oLSaqUn_AmpPfD_5wekBhbRu02Lmvub7XePbSl2mR5c1ee5F2JRJc4ZVj9RUXc1KQkq6jFyrF1OFxdeDKLJL9NNIv5c7MLp8YU_lnDunjnF6UJj8_AEb7iee8IZ9onkROFrjwgaI4b4C5pFk7XV5mh9JR6amcrY_EhIFVpd7o1FOylpzDHe7Q'
+vk = VK(access_token)
+vk.get_photo(62864744)
